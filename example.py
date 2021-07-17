69f9599ddf7a2d3df1d8a0b9dd98cfdbb818b172
Matx00 <Matx00@users.noreply.github.com>
2021-07-17 22:14:35 +0200

    Update example.py

--- example.py
+++ example.py
@@ -6,8 +6,8 @@ from datetime import datetime, timedelta
 
 # Note: for information on how to use this example code please read https://metaapi.cloud/docs/client/usingCodeExamples
 
-token = os.getenv('TOKEN') or 'eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiJhZmE2YWUyMWUzZGY2ZDc3NzVmNGJjMDFiNDlmM2QwNSIsInBlcm1pc3Npb25zIjpbXSwidG9rZW5JZCI6IjIwMjEwMjEzIiwiaWF0IjoxNjI2NTUxNjY4LCJyZWFsVXNlcklkIjoiYWZhNmFlMjFlM2RmNmQ3Nzc1ZjRiYzAxYjQ5ZjNkMDUifQ.B3szVkT3HnTBiiGDknaOaYgicdIiBtJ4UbqUH46ZHZK1GUBBuexYXyiYEE7QQtsaTzXD5LhLTtox-jwo2ZHB2Ym-HV7zh1SaRhCb0BNgVwMYsjhdOVYdxsD0eb1ZPRY5BWXAQYZphpuWl4YrBl10S7jATLXUss5_Ann95zcmOaTnJ2gV-HLnrUWVhzCbs758HD8p5cgSr2nBNCbUcDM72YjA891-Jrg7VWFYk-P4vjUnq32e-PGwhgdQB3zXAIcQUaWNSjFCeRkmUV1CeLWUVk9eA9H3N73-Mafy21WaZ_XuQadrPuY583jVLluXMKQFAj0l_3C7M4WJpqq5NqyXHAn1-C_n4_fcWurwLYWk6IRkUt4nSzl1ATAwQdM7cGofHnt6oSGCHjOe38ZU1CIxKlWfqHuMRvRltYmr0zm4qSlqe0FwHqetICFMpGuTYJDvOBW1XSadRq7MlDUCvKp_01-zmZd-GHb66rYqppN3j9SJCtXKqUXikE25Sr0Cwaef1fF0uhV3QtX8VkDBg4fYKDmRUzQ_L52Aj-ADGXDqaLagi8bgFHP-I_SyA_6OGpOyUpoftr30CZ39QfVpZGHdb3YLkd7UG8S36S84AsivSobotwpcH0Un-qPoFa3xrS5ALO8BbacVNHz15U13inF-ArXF7eRg9ZfWCy_2N56kack'
-accountId = os.getenv('ACCOUNT_ID') or 'f920eee0-258f-4336-972a-8c73ff0ca226'
+token = os.getenv('TOKEN') or '....'
+accountId = os.getenv('ACCOUNT_ID') or '...'
 
 
 async def test_meta_api_synchronization():
